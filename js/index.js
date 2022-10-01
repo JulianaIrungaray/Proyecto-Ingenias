ecc50c6a77505fc11b0d419cb10071773a0d83e7
Cindy Torrealbagit config --global user.name Cindy <cndtorrealba@gmail.com>
2022-10-01 16:43:20 -0300

    agregué función de boton top

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 const edadDelUsuario = parseInt(prompt("¿Cuál es tu edad?"));
 if(edadDelUsuario >= 18){
     alert("Bievenid@ a Vino Lovers ♥");
@@ -18,16 +17,4 @@ function scrollToTop(){
 
 }
 
-=======
-// const edadDelUsuario = parseInt(prompt("¿Cuál es tu edad?"));
-// if(edadDelUsuario >= 18){
-//     alert("Bievenid@ a Vino Lovers ♥");
-// }else{
-//     alert("Eres menor de edad, no puedes ingresar a la página");
-//     var pagina="script.html"
-//     function redireccionar(){
-//         location.href=pagina
-//     }
-//         setTimeout("redireccionar()", 1);
-// }
->>>>>>> 59d60d433d2fba75a105258ea5dfd12ec936b71f
+
