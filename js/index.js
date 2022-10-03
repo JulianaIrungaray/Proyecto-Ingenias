d9b6e00c4c1de958cad4983f61b97aa02dfdefcc
Juliana Irungaray <julianairungaray3@gmail.com>
2022-10-03 20:17:00 -0300

    agregue cursos a la navegacion y contenido textos

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
-
-// const edadDelUsuario = parseInt(prompt("¿Cuál es tu edad?"));
-// if(edadDelUsuario >= 18){
-//     alert("Bievenid@ a Vino Lovers ♥");
-// }else{
-//     alert("Eres menor de edad, no puedes ingresar a la página");
-//     var pagina="script.html"
-//     function redireccionar(){
-//         location.href=pagina
-//     }
-//         setTimeout("redireccionar()", 1);
-// }
+ const edadDelUsuario = parseInt(prompt("¿Cuál es tu edad?"));
+ if(edadDelUsuario >= 18){
+}else{
+     alert("Eres menor de edad, no puedes ingresar a la página");
+     var pagina="script.html"
+     function redireccionar(){
+         location.href=pagina
+     }
+         setTimeout("redireccionar()", 1);
+ }
