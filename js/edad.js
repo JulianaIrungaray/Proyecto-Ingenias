2f2a3584a7037c7821cf5645d7fea9b28eff67d8
Cindy <cndtorrealba@gmail.com>
2022-11-09 08:33:18 -0300

    cambios

--- js/edad.js
+++ js/edad.js
@@ -5,10 +5,10 @@ window.onload = init;
         document.querySelector(".emergente .menor").addEventListener("click",adios);
         document.querySelector(".emergente .mayor").addEventListener("click",hola);
     }
-        
+
     function adios(){
         location.href="errorEdad.html";
-    }    
+    }
     function hola(){//
         document.querySelector(".emergente").style.display="none";
         document.querySelector(".containerEdad").style.opacity="1";
